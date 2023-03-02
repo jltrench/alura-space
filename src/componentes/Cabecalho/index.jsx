c17805b9659a39d9f64fee9e696495c3eaa90555
jltrench <jltrench@gmail.com>
2023-03-02 07:47:33 -0300

    Criando e estilizando componentes, menu e banner

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -1,12 +1,13 @@
 import logo  from './logo.png';
 import search from './search.png';
+import styles from './Cabecalho.module.scss'
 
 export default function Cabecalho() {
     return (
-        <header>
+        <header className={styles.cabecalho}>
             <img src={logo} alt="Logo"/>
-            <div>
-                <input type="text" placeholder="O que voce procura?"/>
+            <div className={styles.cabecalho__container}>
+                <input className={styles['cabecalho__input']} type="text" placeholder="O que voce procura?"/>
                 <img src={search} alt="Icone de lupa" />
             </div>
         </header>
