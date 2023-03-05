5f7e68539cbf9676db1e5dbdde24f05eea19a626
jltrench <jltrench@gmail.com>
2023-03-05 16:36:25 -0300

    Criando componente Galeria

---
+++ src/componentes/Galeria/index.jsx
@@ -0,0 +1,12 @@
+import styles from 'Galeria.module.scss';
+
+import React from 'react'
+
+export default function Galeria() {
+  return (
+    <section>
+        <h2>Navegue pela galeria</h2>
+        <ul></ul>
+    </section>
+  )
+}
