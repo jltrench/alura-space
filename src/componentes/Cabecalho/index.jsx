572c3b9186de3df32b33fed53f0ebf12cea7454f
jltrench <jltrench@gmail.com>
2023-03-06 14:09:14 -0300

    Corringindo placeholder input

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -7,7 +7,7 @@ export default function Cabecalho() {
         <header className={styles.cabecalho}>
             <img src={logo} alt="Logo"/>
             <div className={styles.cabecalho__container}>
-                <input className={styles['cabecalho__input']} type="text" placeholder="O que voce procura?"/>
+                <input className={styles['cabecalho__input']} type="text" placeholder="O que você procura?"/>
                 <img src={search} alt="Icone de lupa" />
             </div>
         </header>
